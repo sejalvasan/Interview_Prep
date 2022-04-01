1a7111436c91d163fc13a6ce41c9b04475827a8e
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-04-01 16:03:21 +0530

    Time: 3 ms (44.72%), Space: 58.2 MB (18.72%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,27 +1,44 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-    int n = nums.length;
-        int[]prefix=new int[n];
-        int[]suffix = new int[n];
+        //T(C)= O(N) SC=O(N)
+//     int n = nums.length;
+//         int[]prefix=new int[n];
+//         int[]suffix = new int[n];
         
-        prefix[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]*nums[i];
+//         prefix[0]=nums[0];
+//         for(int i=1;i<n;i++){
+//             prefix[i]=prefix[i-1]*nums[i];
+//         }
+        
+//          suffix[n-1]=nums[n-1];
+//         for(int i=n-2;i>=0;i--){
+//             suffix[i]=suffix[i+1]*nums[i];
+//         }
+        
+//         int[]arr = new int[n];
+        
+//         arr[0]=suffix[1];
+//         arr[n-1]=prefix[n-2];
+        
+//         for(int i =1;i<n-1;i++){
+//             arr[i]=prefix[i-1]*suffix[i+1];
+//         }
+//         return arr;
+        
+        //TC O(N) SC O(1)
+        int n = nums.length;
+        int suffixProd =1;
+        int[]prefix = new int[n];
+         prefix[0]=nums[0];
+        for(int i=1;i<n;i++)
+           prefix[i]=prefix[i-1]*nums[i];
+        
+        for(int i =n-1;i>0;i--){
+            prefix[i]=prefix[i-1]*suffixProd;
+            suffixProd*=nums[i];
         }
+        prefix[0]=suffixProd;
         
-         suffix[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=suffix[i+1]*nums[i];
-        }
-        
-        int[]arr = new int[n];
-        
-        arr[0]=suffix[1];
-        arr[n-1]=prefix[n-2];
-        
-        for(int i =1;i<n-1;i++){
-            arr[i]=prefix[i-1]*suffix[i+1];
-        }
-        return arr;
+        return prefix;
     }
 }
