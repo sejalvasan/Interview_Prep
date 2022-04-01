a30baacd386e01f5eff04ff68c2909523bfb8432
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-04-01 13:38:03 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (40.90%) - LeetHub

---
+++ 556-next-greater-element-iii/556-next-greater-element-iii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int nextGreaterElement(int n) {
+        char arr[] = String.valueOf(n).toCharArray();
+        int l = arr.length;
+        int i = l-2,j=l-1;
+        while(i>=0 && arr[i]>=arr[i+1])
+            i--;
+        if(i<0)
+            return -1;
+        while(j>=0 && arr[j]<=arr[i])
+            j--;
+        if(i>=0)
+            swap(arr,i,j);
+        
+        reverse(arr, i+1, l-1);
+        
+       long val = Long.valueOf(String.valueOf(arr));
+        return val <= Integer.MAX_VALUE ? (int) val : -1; 
+    }
+    private void swap(char[] array, int i, int j) {
+        char tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+    
+    public static void reverse(char[]array, int i , int j){
+        while(i<j){
+        char tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+}
