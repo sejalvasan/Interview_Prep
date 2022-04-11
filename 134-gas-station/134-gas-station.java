e289c2a878f4460c6052b0c625ec661fef47615f
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-04-11 12:03:08 +0530

    Time: 4 ms (28.68%), Space: 82.6 MB (7.29%) - LeetHub

---
+++ 134-gas-station/134-gas-station.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        
+        int n = gas.length;
+
+        int totgas = 0;
+        int totcost = 0;
+        for(int i =0;i<n;i++){
+            totgas+=gas[i];
+            totcost+=cost[i];
+        }       
+        
+        if(totcost>totgas)
+            return -1;
+        
+        int [] delta = new int[n];
+        for(int i=0;i<n;i++){
+            delta[i]=gas[i]-cost[i];
+        }
+        
+        int[]deltasum = new int[n];
+        deltasum[0]=delta[0];
+        for(int i =1;i<n;i++){
+            deltasum[i]=deltasum[i-1]+delta[i];
+        }
+        
+        int idx=0;
+        int min =Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(deltasum[i]<min){
+                min=deltasum[i];
+                idx=i;
+            }
+        }
+        if(idx!=n-1)
+        return idx+1;
+        
+        return 0;
+    }
+}
