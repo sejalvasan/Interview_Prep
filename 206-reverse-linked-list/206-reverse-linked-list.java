6f04b1ad3001d8e7e618dfd433f6ebb107cce0ef
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-04-12 22:34:04 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (38.44%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+//         if(head == null || head.next ==null)
+//             return head;
+        
+//         ListNode newHead = reverseList(head.next);
+        
+//         head.next.next =head;
+        
+//         head.next = null;
+        
+//         return newHead;
+        
+        
+        
+        ListNode prev =null, cur = head, next =null;
+        while(cur!=null){
+        next=cur.next;
+        cur.next=prev;
+        
+        prev = cur;
+        cur=next;
+            
+        }
+        
+        head = prev;
+        
+        return head;
+    }
+}
