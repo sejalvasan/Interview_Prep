1434c4e65ab7b6349b93c2de64b0208115b75f5e
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-04-29 18:35:36 +0530

    Added solution - LeetHub

---
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.java
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String input_line[] = in.readLine().trim().split("\\s+");
+            int Matrix[][] = new int[N][N];
+            for(int i = 0; i < N*N; i++)
+                Matrix[(i/N)][i%N] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.maximumPath(N, Matrix));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+   static int maximumPath(int N, int Matrix[][])
+    {
+        // code here
+       
+        for(int i =1;i<N;i++){
+            for(int j =0;j<N;j++){
+             if(j>0 && j<N-1)
+             Matrix[i][j]=Matrix[i][j]+Math.max(Matrix[i-1][j],Math.max(Matrix[i-1][j+1],Matrix[i-1][j-1]));
+             
+             else if(j==N-1)
+                 Matrix[i][j]=Matrix[i][j]+Math.max(Matrix[i-1][j],Matrix[i-1][j-1]);
+        
+           else if(j==0)
+           Matrix[i][j]=Matrix[i][j]+Math.max(Matrix[i-1][j],Matrix[i-1][j+1]);
+       
+            }
+        }
+          int maxi = 0;
+        for(int i = 0;i < N;i++)
+        maxi = Math.max(maxi, Matrix[N-1][i]);
+        return maxi;
+    }
+}
