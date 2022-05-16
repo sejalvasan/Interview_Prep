4dcc3e58973e597d4493ddb338dec5aa1a546fd4
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-05-16 15:58:29 +0530

    Time: 28 ms (39.55%), Space: 64.6 MB (40.70%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -0,0 +1,52 @@
+class Pair{
+    int x;
+    int y;
+    Pair(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+class Solution {
+    public int maxDistance(int[][] grid) {
+        Queue<Pair> q = new LinkedList<>();
+        
+        int level =-1;
+                int zeroCount=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1)
+                    q.add(new Pair(i,j));
+                else{
+                    zeroCount++;
+                }
+            }
+        }
+        if(zeroCount ==0)
+            return -1;
+        int[][]dir = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            
+            level++;
+            while(size-->0){
+                Pair rem = q.remove(); 
+                for(int k =0;k<4;k++){
+                    int row = dir[k][0]+rem.x;
+                    int col = dir[k][1]+rem.y;
+                    
+                    if(row>=0 && col>=0 && row<grid.length && col<grid[0].length && grid[row][col]==0 ){
+                        q.add(new Pair(row,col));
+                        zeroCount--;
+                        grid[row][col]=1;
+                    }
+                }
+            }
+        }
+        if(zeroCount>0)
+            return -1;
+        
+        return level;
+    }
+}
