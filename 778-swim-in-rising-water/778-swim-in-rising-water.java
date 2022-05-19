cf10e7276c1be4d59168c89479e770d4c49c2361
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-05-19 18:52:20 +0530

    Time: 14 ms (61.22%), Space: 46.1 MB (33.15%) - LeetHub

---
+++ 778-swim-in-rising-water/778-swim-in-rising-water.java
@@ -0,0 +1,49 @@
+public class Pair implements Comparable<Pair>{
+    int row;
+    int col;
+    int val;
+    
+    Pair(int row, int col, int val){
+        this.row = row;
+        this.col=col;
+        this.val=val;
+    }
+    @Override
+    public int compareTo(Pair o){
+       return this.val - o.val;
+    }
+}
+class Solution {
+    public int swimInWater(int[][] grid) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(0,0,grid[0][0]));
+        int n = grid.length;
+        boolean[][] vis = new boolean[n][n];
+        int[][]dir = new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
+        
+        while(!pq.isEmpty()){
+        Pair rem = pq.remove();
+            if(vis[rem.row][rem.col] == true)
+                continue;
+            if(rem.row ==n-1 && rem.col==n-1)
+                return rem.val;
+            
+            if(vis[rem.row][rem.col]==true)
+                continue;
+            
+            vis[rem.row][rem.col]=true;
+            
+            for(int i=0;i<4;i++){
+                int rowdash = rem.row+ dir[i][0];
+                int coldash = rem.col + dir[i][1];
+                
+                if(rowdash<0 || coldash<0 || rowdash>=n || coldash>=n || vis[rowdash][coldash]==true)
+                    continue;
+                
+                pq.add(new Pair(rowdash,coldash,Math.max(grid[rowdash][coldash], rem.val)));
+            }
+        
+        }
+        return 0;
+    }
+}
