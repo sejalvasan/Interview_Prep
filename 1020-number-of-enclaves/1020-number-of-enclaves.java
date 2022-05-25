15459c69d05a54093bb49c48122b45a5e66968d4
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-05-25 13:02:31 +0530

    Time: 6 ms (65.27%), Space: 66.1 MB (26.77%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,33 +1,38 @@
 class Solution {
+    static int[][]dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
     public int numEnclaves(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+       
+        for(int i=0;i<n;i++){
+         for(int j=0;j<m;j++){
+             if(i==0 || j==0 || i==n-1 || j==m-1)
+             if(grid[i][j]==1){
+                 dfs(i,j, grid);
+             }
+         }   
+        }
         
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(i==0|| i==grid.length-1|| j==0 || j== grid[0].length-1){
-                    if(grid[i][j]==1)
-                        dfs(grid,i,j);
-                }
+        int count =0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1)
+                   count++;
             }
         }
-        int count=0;
-   for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-            if(grid[i][j]==1)
-                count++;
-            }
-   }
         return count;
-            }
-    
-    public static void dfs(int grid[][], int row, int col){
-     if(row<0|| row>=grid.length|| col<0 || col>=grid[0].length ||grid[row][col]==0)
-     return;
-  
-     grid[row][col]=0;
-        
-        dfs(grid,row+1,col);
-        dfs(grid,row-1,col);
-        dfs(grid,row,col+1);
-        dfs(grid,row,col-1);
     }
+    
+    public static void dfs(int r, int c , int[][]grid){
+        grid[r][c]=0;
+       for(int i=0;i<4;i++){
+            int nr = r+dirs[i][0];
+            int nc = c+dirs[i][1];
+                
+                if(nr<0 || nc<0 || nr>=grid.length || nc>= grid[0].length || grid[nr][nc]==0)
+                    continue;
+                grid[nr][nc]=0;
+           dfs(nr, nc, grid);
+            }
+        }
 }
