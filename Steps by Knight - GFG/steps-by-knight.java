66dab30e91f4c3826e602dfcc6fb898d7a190930
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-05-26 17:03:33 +0530

    Added solution - LeetHub

--- Steps by Knight - GFG/steps-by-knight.java
+++ Steps by Knight - GFG/steps-by-knight.java
@@ -27,108 +27,54 @@ class GFG
 }
 // } Driver Code Ends
 
-// class Pair{
-//     int x;
-//     int y;
-//     Pair(int x, int y){
-//         this.x =x;
-//         this.y =y;
-//     }
-// }
-// class Solution
-// {
-//     static int[][]dirs = new int[][]{{1,2},{2,1},{-1,2},{2,-1},{1,-2},{-2,1}
-//     ,{-1,-2},{-2,-1}};
-//     public int minStepToReachTarget(int KnightPos[], int TargetPos[], int N)
-//     {
-//         // Code here
-//         //0 based indexing
-//         KnightPos[0]--;
-// 		KnightPos[1]--;
-// 		TargetPos[0]--;
-// 		TargetPos[1]--;
-//         Queue<Pair> q = new LinkedList<>();
-//         int steps =0;
-//         q.add(new Pair(KnightPos[0],TargetPos[1]));
-//         boolean[][]vis = new boolean[N][N];
-//         int s1=KnightPos[0];
-//         int s2 = KnightPos[1];
-//         vis[s1][s2]=true;
-        
-        
-//         while(!q.isEmpty()){
-//             int size = q.size();
-//             while(size-->0){
-//                 Pair rem = q.remove();
-//                 if(rem.x == TargetPos[0] && rem.y == TargetPos[1])
-//                 return steps;
-                
-//                 for(int i=0;i<8;i++){
-//                     int r = rem.x+dirs[i][0];
-//                     int c = rem.y+dirs[i][1];
-                    
-//                     if(r<0 || c<0 || r>=N || c>=N || vis[r][c]==true)
-//                     continue;
-                    
-//                     vis[r][c]=true;
-//                     q.add(new Pair(r,c));
-//                 }
-//             }
-//                  steps++;
-//         }
-//         return -1;
-//     }
-// }
-
 class Pair{
-    int x, y;
-    
-    Pair(int a, int b){
-        x = a;
-        y = b;
+    int x;
+    int y;
+    Pair(int x, int y){
+        this.x =x;
+        this.y =y;
     }
 }
-
 class Solution
 {
-    int ans;
-    int dir[][] = {{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}};
-    //Function to find out minimum steps Knight needs to reach target position.
+    static int[][]dirs = new int[][]{{1,2},{2,1},{-1,2},{2,-1},{1,-2},{-2,1}
+    ,{-1,-2},{-2,-1}};
     public int minStepToReachTarget(int KnightPos[], int TargetPos[], int N)
     {
         // Code here
-        ans = Integer.MAX_VALUE;
-        boolean[][] vis = new boolean[N+1][N+1]; // one based indexing
-        
-        // BACKTRACKING SOL
-        // WORKING - But took too long
-        // minStepToReachTarget(KnightPos[0], KnightPos[1], TargetPos, N, vis, 0);
-        
-        // GRAPHS - BFS
-        // Strategy: Add nodes at distance 1 the all nodes at distance 2, ..3
+        //0 based indexing
+        KnightPos[0]--;
+		KnightPos[1]--;
+		TargetPos[0]--;
+		TargetPos[1]--;
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(KnightPos[0], KnightPos[1]));
-        int level = 0;
+        int steps =0;
+        q.add(new Pair(KnightPos[0],KnightPos[1]));
+        boolean[][]vis = new boolean[N][N];
+        int s1=KnightPos[0];
+        int s2 = KnightPos[1];
+        vis[s1][s2]=true;
+        
+        
         while(!q.isEmpty()){
             int size = q.size();
             while(size-->0){
-                Pair fn = q.remove();
-                if(fn.x == TargetPos[0] && fn.y == TargetPos[1]){
-                    return level;
-                }
+                Pair rem = q.remove();
+                if(rem.x == TargetPos[0] && rem.y == TargetPos[1])
+                return steps;
                 
-                if(vis[fn.x][fn.y]) continue;
-                
-                vis[fn.x][fn.y] = true;
-                
-                for(int k = 0; k < 8; k++){
-                    int row = fn.x + dir[k][0];
-                    int col = fn.y + dir[k][1];
-                    if(row < 0 || col < 0 || row > N || col > N || vis[row][col]) continue;
-                    q.add(new Pair(row, col));
+                for(int i=0;i<8;i++){
+                    int r = rem.x+dirs[i][0];
+                    int c = rem.y+dirs[i][1];
+                    
+                    if(r<0 || c<0 || r>=N || c>=N || vis[r][c]==true)
+                    continue;
+                    
+                    vis[r][c]=true;
+                    q.add(new Pair(r,c));
                 }
             }
-            level++; 
+                 steps++;
         }
         return -1;
     }
