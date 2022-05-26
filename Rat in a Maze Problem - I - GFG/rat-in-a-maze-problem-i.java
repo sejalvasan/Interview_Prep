512d8eb795b53ac2b53eb19b33c348314388b650
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-05-26 11:27:05 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
@@ -33,7 +33,8 @@ class Rat {
 
 // m is the given matrix and n is the order of matrix
 class Solution {
-    private static void solve(int i, int j, int a[][], int n, ArrayList<String> ans, String move, 
+    private static void solve(int i, int j, int a[][], int n, 
+    ArrayList<String> ans, String move, 
     int vis[][]) {
         if(i==n-1 && j==n-1) {
             ans.add(move);
@@ -56,29 +57,26 @@ class Solution {
         if(i+1<n && vis[i+1][j] == 0 && a[i+1][j] == 1) {
             vis[i][j] = 1; 
             solve(i+1, j, a, n, ans, move + 'D', vis);
-            vis[i][j] = 0; 
         }
         
         // left
         if(j-1>=0 && vis[i][j-1] == 0 && a[i][j-1] == 1) {
             vis[i][j] = 1; 
             solve(i, j-1, a, n, ans, move + 'L', vis);
-            vis[i][j] = 0; 
         }
         
         // right 
         if(j+1<n && vis[i][j+1] == 0 && a[i][j+1] == 1) {
             vis[i][j] = 1; 
             solve(i, j+1, a, n, ans, move + 'R', vis);
-            vis[i][j] = 0; 
         }
         
         // upward
         if(i-1>=0 && vis[i-1][j] ==0 && a[i-1][j] == 1) {
             vis[i][j] = 1; 
             solve(i-1, j, a, n, ans, move + 'U', vis);
-            vis[i][j] = 0; 
         }
+          vis[i][j] = 0; 
     }
     public static ArrayList<String> findPath(int[][] m, int n) {
         int vis[][] = new int[n][n];
