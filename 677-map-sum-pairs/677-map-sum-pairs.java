c52826b626c5c0ad76332bad945d522294a2eb0f
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-03 15:26:19 +0530

    Time: 15 ms (68.25%), Space: 42.7 MB (63.12%) - LeetHub

--- 677-map-sum-pairs/677-map-sum-pairs.java
+++ 677-map-sum-pairs/677-map-sum-pairs.java
@@ -1,55 +1,75 @@
 class MapSum {
-    
-    class TrieNode{
-        TrieNode[]children = null;
-        int wt=0;
-        
-        TrieNode(){
-            children = new TrieNode[26];
-        }
+    HashMap<String, Integer> map = null;
+    public MapSum() {
+        map = new HashMap<>();
     }
-        TrieNode root = null;
-
-    public MapSum(){
-        root = new TrieNode();
-    }
-    
     
     public void insert(String key, int val) {
-        TrieNode curr = root;
-        for(char c:key.toCharArray()){
-            int idx = c-'a';
-            if(curr.children[idx]==null)
-                curr.children[idx]=new TrieNode();
-            
-            curr = curr.children[idx];
-        }
-        curr.wt = val;
+        map.put(key,val);
     }
     
     public int sum(String prefix) {
-        TrieNode curr = root;
-        for(char c:prefix.toCharArray()){
-            int idx = c-'a';
-            if(curr.children[idx]==null)
-                return 0;
-            
-            curr = curr.children[idx];
+        int total =0;
+        for(Map.Entry<String, Integer> entry: map.entrySet()){
+            if(entry.getKey().startsWith(prefix)){
+                total+=entry.getValue();
+            }
         }
-        return dfs(curr);
-    }
-    
-    public int dfs(TrieNode curr){
-        int sum =0;
-        for(int i=0;i<26;i++){
-            if(curr.children[i]!=null)
-                sum+=dfs(curr.children[i]);
-        }
-        
-        return sum + curr.wt;
+        return total;
     }
 }
 
+// class MapSum {
+    
+//     class TrieNode{
+//         TrieNode[]children = null;
+//         int wt=0;
+        
+//         TrieNode(){
+//             children = new TrieNode[26];
+//         }
+//     }
+//         TrieNode root = null;
+
+//     public MapSum(){
+//         root = new TrieNode();
+//     }
+    
+    
+//     public void insert(String key, int val) {
+//         TrieNode curr = root;
+//         for(char c:key.toCharArray()){
+//             int idx = c-'a';
+//             if(curr.children[idx]==null)
+//                 curr.children[idx]=new TrieNode();
+            
+//             curr = curr.children[idx];
+//         }
+//         curr.wt = val;
+//     }
+    
+//     public int sum(String prefix) {
+//         TrieNode curr = root;
+//         for(char c:prefix.toCharArray()){
+//             int idx = c-'a';
+//             if(curr.children[idx]==null)
+//                 return 0;
+            
+//             curr = curr.children[idx];
+//         }
+//         return dfs(curr);
+//     }
+    
+//     public int dfs(TrieNode curr){
+//         int sum =0;
+//         for(int i=0;i<26;i++){
+//             if(curr.children[i]!=null)
+//                 sum+=dfs(curr.children[i]);
+//         }
+        
+//         return sum + curr.wt;
+//     }
+// }
 /**
  * Your MapSum object will be instantiated and called as such:
  * MapSum obj = new MapSum();
