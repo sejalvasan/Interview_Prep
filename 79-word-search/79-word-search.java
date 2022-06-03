6fd07aaa7e5903a345552591c3aef96c8b9a8394
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-03 15:43:47 +0530

    Time: 149 ms (56.96%), Space: 42.4 MB (40.98%) - LeetHub

---
+++ 79-word-search/79-word-search.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        return exists(board,word);
+      }
+    
+    public static boolean exists(char board[][], String word){
+        int n = board.length;
+        int m = board[0].length;
+        int idx =0;
+        boolean[][]vis = new boolean[n][m];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word.charAt(idx)){
+                   if( dfs(board, word,vis,i, j, idx))
+                       return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public static boolean dfs(char board[][], String word, boolean[][]visited, int row, int col, int idx){
+          
+        if(idx==word.length())
+        return true;
+        
+        if(row>=0 && row<board.length && col>=0 && col<board[0].length 
+        && visited[row][col]==false && board[row][col]==word.charAt(idx)){
+           
+            visited[row][col]=true;
+            
+           if(
+           dfs(board,word,visited,row,col+1, idx+1) ||
+           dfs(board,word,visited,row+1,col, idx+1) ||
+           dfs(board,word,visited,row,col-1, idx+1) ||
+           dfs(board,word,visited,row-1,col, idx+1))
+           
+           return true;
+           
+           //bacltrack
+           visited[row][col]=false;
+        }
+        return false;
+        
+    }}
