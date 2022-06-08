0fbb396ee1a46d217aba06e3e3bf2d3b779ed5c5
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-08 17:50:52 +0530

    Time: 0 ms (100.00%), Space: 49.8 MB (51.62%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -15,12 +15,41 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
+        //brute  TC = O(N)
+//         if(root==null)
+//             return 0;
+        
+//         int ls = countNodes(root.left);
+//         int rs = countNodes(root.right);
+        
+//         return ls+rs+1;
+        
+        
+        //OPTIMISED TC = O(LOGN)^2
+        
         if(root==null)
             return 0;
+        int lh=0;
+        TreeNode node = root.left;
         
-        int ls = countNodes(root.left);
-        int rs = countNodes(root.right);
+        while(node!=null){
+            node = node.left;
+            lh++;
+        }
         
-        return ls+rs+1;
+        int rh=0;
+         node = root.right;
+        
+        while(node!=null){
+            node = node.right;
+            rh++;
+        }
+        
+        if(lh==rh){
+            int ht = lh+1;
+            return (1<< ht)-1;
+        }else{
+            return countNodes(root.left)+ countNodes(root.right)+1;
+        }
     }
 }
