aa140b6cda702f8af5210571db17c055239fd227
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-09 17:22:27 +0530

    Time: 2 ms (100.00%), Space: 41.6 MB (99.00%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -14,30 +14,71 @@
  * }
  */
 class Solution {
-       int count =0;
+    
+    //O(N^2)
+//        int count =0;
+//     public int pathSum(TreeNode root, int targetSum) {
+//         travel1(root,targetSum);
+//         return count;
+//     }
+    
+//     public void travel1(TreeNode root, int targetSum){
+//         if(root==null)
+//             return;
+        
+//         travel2(root,0,targetSum);
+        
+//         travel1(root.left,targetSum);
+//         travel1(root.right,targetSum);
+//     }
+    
+//     public void travel2(TreeNode root, int csum, int targetSum){
+//         if(root==null)
+//             return;
+        
+//         if(csum+root.val == targetSum)
+//             count++;
+        
+//         travel2(root.left,csum+root.val,targetSum);
+//         travel2(root.right,csum+root.val,targetSum);
+//     }
+// }
+    int count =0;
     public int pathSum(TreeNode root, int targetSum) {
-        travel1(root,targetSum);
+        if(root ==null)
+            return 0;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        helper(root, root.val, targetSum,map);
         return count;
     }
     
-    public void travel1(TreeNode root, int targetSum){
-        if(root==null)
-            return;
+    public void helper(TreeNode node, int psum, int target, HashMap<Integer, Integer> map){
         
-        travel2(root,0,targetSum);
+        if(map.containsKey(psum-target)){
+            count+=map.get(psum-target);
+        }
         
-        travel1(root.left,targetSum);
-        travel1(root.right,targetSum);
-    }
-    
-    public void travel2(TreeNode root, int csum, int targetSum){
-        if(root==null)
-            return;
+        if(map.containsKey(psum)){
+            map.put(psum,map.get(psum)+1);
+        }else{
+            map.put(psum,1);
+        }
         
-        if(csum+root.val == targetSum)
-            count++;
+        if(node.left!=null){
+            helper(node.left, psum+node.left.val,target,map);
+        }
         
-        travel2(root.left,csum+root.val,targetSum);
-        travel2(root.right,csum+root.val,targetSum);
+        if(node.right!=null){
+            helper(node.right, psum+node.right.val,target,map);
+        }
+        
+        //call back
+        if(map.get(psum)==1)
+            map.remove(psum);
+        else{
+            map.put(psum,map.get(psum)-1);
+        }
     }
 }
