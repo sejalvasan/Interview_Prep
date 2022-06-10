bfd6e7d445bc8fa5f485161e19c1a764a5dde94d
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-10 17:24:36 +0530

    Time: 1 ms (45.76%), Space: 42.5 MB (19.84%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -13,6 +13,15 @@
  *     }
  * }
  */
+
+class Pair{
+    TreeNode node;
+    int state;
+    Pair(TreeNode node,int state){
+        this.node = node;
+        this.state = state;
+    }
+}
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         //using 1 stack
@@ -43,29 +52,54 @@ class Solution {
 // }
         
      //using 2 stacks
-        Stack<TreeNode> st1 = new Stack<>();
-        Stack<TreeNode> st2 = new Stack<>();
-        List<Integer> res = new ArrayList<>();
+//         Stack<TreeNode> st1 = new Stack<>();
+//         Stack<TreeNode> st2 = new Stack<>();
+//         List<Integer> res = new ArrayList<>();
         
+//         if(root ==null)
+//             return res;
+//         st1.add(root);
+        
+//         while(!st1.isEmpty()){
+//           TreeNode node = st1.pop();
+//             st2.add(node);
+            
+//             if(node.left!=null)
+//                 st1.add(node.left);
+            
+//             if(node.right!=null)
+//                 st1.add(node.right);
+            
+//         }
+        
+//         while(!st2.isEmpty())
+//             res.add(st2.pop().val);
+        
+//         return res;
+        List<Integer> post = new ArrayList<>();
+               Stack<Pair> st = new Stack<>();
+               st.add(new Pair(root,1));
         if(root ==null)
-            return res;
-        st1.add(root);
-        
-        while(!st1.isEmpty()){
-          TreeNode node = st1.pop();
-            st2.add(node);
-            
-            if(node.left!=null)
-                st1.add(node.left);
-            
-            if(node.right!=null)
-                st1.add(node.right);
-            
+            return post;
+        while(!st.isEmpty()){
+            Pair top = st.peek();
+            if(top.state==1){
+                top.state++;
+                if(top.node.left!=null){
+                    Pair lp = new Pair(top.node.left,1);
+                    st.add(lp);
+                }
+            }else if(top.state==2){
+                top.state++;
+                if(top.node.right!=null){
+                    Pair rp = new Pair(top.node.right,1);
+                    st.add(rp);
+                }
+            }else{
+                post.add(top.node.val);
+                st.pop();
+            }
         }
-        
-        while(!st2.isEmpty())
-            res.add(st2.pop().val);
-        
-        return res;
+        return post;
     }
 }
