0bd6b640877d410ce286472e396619794c5372fc
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-14 17:07:29 +0530

    Time: 2 ms (15.55%), Space: 47.8 MB (41.62%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -17,25 +17,38 @@ class Solution {
    int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
         max = Integer.MIN_VALUE;
-        maxPathUtil(root);
+       helper(root);
         return max;
     }
     
-    public int maxPathUtil(TreeNode root){
-        if(root==null)
-            return 0;
-        
-        int left = maxPathUtil(root.left);
-        int right = maxPathUtil(root.right);
-        
-        int ldash = Math.max(left,0);
-        int rdash = Math.max(right,0);
-        
-        int leftToRight = ldash+rdash+root.val;
-        if(max<leftToRight)
-            max =leftToRight;
-        
-        return Math.max(ldash,rdash)+root.val;
-        
+     int helper(TreeNode node){
+       
+        if(node.left!=null && node.right!=null){
+            int left = helper(node.left);
+            int right = helper(node.right);
+            int ldash = Math.max(0,left);
+            int rdash = Math.max(0,right);
+            
+            max = Math.max(ldash+rdash+node.val,max);
+            
+            return Math.max(ldash,rdash)+node.val;
+            
+        }else if(node.left!=null){
+            int left = helper(node.left);
+            int ldash = Math.max(0,left);
+           max = Math.max(ldash+node.val,max);
+
+            return ldash+node.val;
+            
+        }else if(node.right!=null){
+            int right = helper(node.right);
+            int rdash = Math.max(0,right);
+            max = Math.max(rdash+node.val,max);
+
+            return rdash+node.val;
+        }else{
+            if(max<node.val) max = node.val;
+        return node.val;
+        }
     }
 }
