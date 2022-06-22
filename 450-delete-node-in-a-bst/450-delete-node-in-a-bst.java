acae9ce4872c14e4968cb7859bf47f2a392e8264
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-22 22:06:25 +0530

    Time: 1 ms (14.64%), Space: 49.7 MB (44.22%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -15,31 +15,33 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root==null)
-            return null;
+      if(root==null){
+          return root;
+      }   
         
         if(key<root.val){
-            root.left = deleteNode(root.left, key);
+          root.left =  deleteNode(root.left,key);
             return root;
-        }else if(key>root.val){
-            root.right = deleteNode(root.right, key);
+        }else if(root.val<key){
+              root.right =   deleteNode(root.right,key);
             return root;
         }else{
-           if(root.left!=null && root.right!=null){
-               int lmax = max(root.left);
-               root.val = lmax;
-               root.left = deleteNode(root.left,lmax);
-               return root;
-           }else if(root.left!=null){
-               return root.left;
-           }else if(root.right!=null){
-               return root.right;
-           }else
-               return null;
+            if(root.left!=null && root.right!=null){
+                int lmax = max(root.left);
+                root.val = lmax;
+                root.left = deleteNode(root.left,lmax);
+                return root;
+            }else if(root.left!=null){
+                return root.left;
+            }else if(root.right!=null){
+                return root.right;
+            }else{
+                return null;
+            }
         }
     }
-    
-    public int max(TreeNode root){
+        
+ public int max(TreeNode root){
         if(root.right!=null)
             return max(root.right);
         else
