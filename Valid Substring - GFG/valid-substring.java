9991bb3ba7b9a81d74127d0aae22e7fab4254028
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-25 20:15:27 +0530

    Added solution - LeetHub

---
+++ Valid Substring - GFG/valid-substring.java
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+
+            Solution ob = new Solution();
+            System.out.println(ob.findMaxLen(S));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static int findMaxLen(String S) {
+        // code here
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        int maxL =0;
+        
+     for(int i =0;i<S.length();i++){
+        char ch = S.charAt(i);
+        
+        if(ch=='(')
+        st.push(i);
+        else{
+            if(!st.isEmpty())
+            st.pop();
+            
+            if(!st.isEmpty())
+            maxL = Math.max(maxL, i-st.peek());
+            
+            else
+            st.push(i);
+        }
+     }
+     return maxL;
+    }
+};
