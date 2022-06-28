bde03c78b680566b6eb162471e36f55f67dcffe0
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-28 14:02:33 +0530

    Added solution - LeetHub

--- Stack using two queues - GFG/stack-using-two-queues.java
+++ Stack using two queues - GFG/stack-using-two-queues.java
@@ -69,5 +69,25 @@ class Queues
 	   
 	   // return val;
     // }
+    //pop eff
+     void push(int a){
+         
+         while(!q1.isEmpty())
+         q2.add(q1.remove());
+         
+         q1.add(a);
+         
+         while(!q2.isEmpty())
+         q1.add(q2.remove());
+         
+     }
+     int pop(){
+         if(q1.isEmpty())
+         return -1;
+         
+         return q1.remove();
+         
+     }
+
 }
 
