63187bcbbefb74d25bda7c9f444d00da995d4339
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-28 22:24:44 +0530

    Added solution - LeetHub

---
+++ K largest elements - GFG/k-largest-elements.java
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0)
+        {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            
+            int arr[] = new int[n];
+            for(int i = 0; i<n; ++i)
+                arr[i] = sc.nextInt();
+            Solution ob = new Solution();
+            ArrayList<Integer> list = ob.kLargest(arr, n, k);
+            for(int i = 0; i<list.size(); i++)
+                System.out.print(list.get(i) + " ");
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to return k largest elements from an array.
+    public static ArrayList<Integer> kLargest(int arr[], int n, int k)
+    {
+        // code here 
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int i =0;
+        for( i=0;i<k;i++){
+            pq.add(arr[i]);
+        }
+        
+        for(;i<n;i++){
+            pq.add(arr[i]);
+            pq.remove();
+        }
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        while(!pq.isEmpty()){
+        ans.add(pq.remove());    
+        }
+        
+        Collections.reverse(ans);
+        return ans;
+    }
+}
