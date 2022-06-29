df46c8b2697006d8ce358233b5394881847fcf84
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-29 13:44:40 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.java
+++ Merge Without Extra Space - GFG/merge-without-extra-space.java
@@ -40,21 +40,58 @@ class GFG {
 
 class Solution {
 
-    public void merge(int arr1[], int arr2[], int n, int m) {
+ //   public void merge(int arr1[], int arr2[], int n, int m) {
         // code here
-        int i =n-1;
-        int j =0;
+        // int i =n-1;
+        // int j =0;
         
-        while(i>=0 && j<m){
-            if(arr1[i]>arr2[j]){
-                int temp = arr1[i];
-                arr1[i]=arr2[j];
-                arr2[j]=temp;
+        // while(i>=0 && j<m){
+        //     if(arr1[i]>arr2[j]){
+        //         int temp = arr1[i];
+        //         arr1[i]=arr2[j];
+        //         arr2[j]=temp;
+        //     }
+        //     i--;
+        //     j++;
+        // }
+        // Arrays.sort(arr1);
+        // Arrays.sort(arr2);
+        
+        public int nextGap(int gap) {
+        if (gap <= 1) return 0;
+        return (gap / 2) + (gap % 2);
+    }
+
+    public void merge(int arr1[], int arr2[], int n, int m) {
+        int i, j, gap = n + m, tmp;
+        for (gap = nextGap(gap); gap > 0; gap = nextGap(gap)) {
+            for (i = 0; i + gap < n; i++) {
+                if (arr1[i] > arr1[i + gap]) {
+                    tmp = arr1[i];
+                    arr1[i] = arr1[i + gap];
+                    arr1[i + gap] = tmp;
+                }
             }
-            i--;
-            j++;
+
+            for (j = gap > n ? gap - n : 0; i < n && j < m; i++, j++) {
+                if (arr1[i] > arr2[j]) {
+                    tmp = arr1[i];
+                    arr1[i] = arr2[j];
+                    arr2[j] = tmp;
+                }
+            }
+
+            if (j < m) {
+                for (j = 0; j + gap < m; j++) {
+                    if (arr2[j] > arr2[j + gap]) {
+                        tmp = arr2[j];
+                        arr2[j] = arr2[j + gap];
+                        arr2[j + gap] = tmp;
+                    }
+                }
+            }
         }
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
     }
 }
+
+  
