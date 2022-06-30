22d890e4cee02dbaba8ecd2d5139398ab9a65380
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-06-30 23:28:19 +0530

    Added solution - LeetHub

---
+++ Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.java
@@ -0,0 +1,80 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Driver
+{
+    public static void main(String args[]) 
+	{ 
+	    Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int []a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            
+            int  m= sc.nextInt();
+            int []b = new int[m];
+            for (int i = 0; i < m; i++) b[i] = sc.nextInt();
+            
+            double res = new GFG().medianOfArrays(n, m, a, b);
+            
+            if (res == (int)res) System.out.println ((int)res);
+            else System.out.println (res);
+        }
+    		
+	} 
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class GFG 
+{ 
+    static double medianOfArrays(int n, int m, int a[], int b[]) 
+    {
+        // Your Code Here
+      double res;
+        if (n < m)
+            res = getMedian (n, m, a, b);
+        else
+            res = getMedian (m, n, b, a);
+        
+        return res;
+    }
+    
+    	static double getMedian(int n, int m, int nums1[],int nums2[])
+{
+    
+        int n1 = nums1.length;
+        int n2 = nums2.length; 
+        int low = 0, high = n1;
+        
+        while(low <= high) {
+            int cut1 = (low+high) >> 1;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1; 
+        
+            int left1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1-1];
+            int left2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2-1]; 
+            
+            int right1 = cut1 == n1 ? Integer.MAX_VALUE : nums1[cut1];
+            int right2 = cut2 == n2 ? Integer.MAX_VALUE : nums2[cut2]; 
+            if(left1 <= right2 && left2 <= right1) {
+                if( (n1 + n2) % 2 == 0 ) 
+                    return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0; 
+                else 
+                    return Math.max(left1, left2); 
+            }
+            else if(left1 > right2) {
+                high = cut1 - 1; 
+            }
+            else {
+                low = cut1 + 1; 
+            }
+        }
+        return 0.0;
+    	}
+}
