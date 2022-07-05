12b04f368e50de52898d2fc370c9a6cda911892f
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-05 22:53:15 +0530

    Time: 83 ms (24.84%), Space: 69.7 MB (21.92%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -1,51 +1,58 @@
 class Trie {
-
-     TrieNode root;
-     class TrieNode{
-        private TrieNode[]children;
-        private boolean isWord;
+   
+    public TrieNode root;
+    
+    private class TrieNode{
+        TrieNode[]children =null;
+        boolean isWord;
         
         public TrieNode(){
             children = new TrieNode[26];
         }
-    } 
+    }
+    
     public Trie() {
-        root = new TrieNode();
+     root = new TrieNode();   
     }
     
     public void insert(String word) {
         TrieNode it = root;
-        for(char c: word.toCharArray()){
-            int idx = c-'a';
-            if(it.children[idx]==null)
-                it.children[idx]= new TrieNode();
         
+        for(char c:word.toCharArray()){
+            int idx = c-'a';
+            if(it.children[idx]==null){
+                it.children[idx]=new TrieNode();
+            }
             it=it.children[idx];
         }
-        it.isWord = true;
+        it.isWord=true;
     }
     
     public boolean search(String word) {
-        TrieNode it = root;
-        for(char c: word.toCharArray()){
+         TrieNode it = root;
+        
+        for(char c:word.toCharArray()){
             int idx = c-'a';
-            if(it.children[idx]==null)
-                return false;
-            else
-                it =it.children[idx];
+            if(it.children[idx]==null){
+               return false;
+            }else{
+            it=it.children[idx];
         }
+       }
         return it.isWord;
     }
     
     public boolean startsWith(String prefix) {
-        TrieNode it = root;
-        for(char c: prefix.toCharArray()){
+       TrieNode it = root;
+        
+        for(char c:prefix.toCharArray()){
             int idx = c-'a';
-            if(it.children[idx]==null)
-                return false;
-            else
-                it = it.children[idx];
+            if(it.children[idx]==null){
+               return false;
+            }else{
+            it=it.children[idx];
         }
+       }
         return true;
     }
 }
