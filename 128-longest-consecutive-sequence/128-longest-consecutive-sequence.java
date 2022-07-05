4b7e145b39b0b2be83f91486c5eabb05e5ea61e9
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-05 09:55:12 +0530

    Time: 478 ms (31.37%), Space: 151.9 MB (24.29%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,32 +1,33 @@
 class Solution {
-    public int longestConsecutive(int[] arr) {
-          HashMap<Integer, Boolean> map = new HashMap<>();
-	   for(int val: arr){
-	       map.put(val,true);
-	   }
-	   
-	   for(int val:arr){
-	       if(map.containsKey(val-1)){
-	           map.put(val,false);
-	       }
-	   }
-	   
-	   int ml=0;
-	   for(int val : arr){
-	       if(map.get(val)==true){
-	           int tl=1;
-	           int tsp=val;
-	           
-	           while(map.containsKey(tsp+tl)){
-	               tl++;
-	           }
-	           
-	           if(tl>ml){
-	               ml=tl;
-	           }
-	 
-	       }
-	   }
-	  return ml;
+    public int longestConsecutive(int[] nums) {
+        HashMap<Integer,Boolean> map = new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],true);
+        }
+        
+         for(int i=0;i<nums.length;i++){
+          if(map.containsKey(nums[i]-1))
+              map.put(nums[i],false);
+        }
+        
+        int maxLen =0;
+        for(int i =0;i<nums.length;i++){
+            if(map.get(nums[i]) == true){
+                
+                int tl = 1;
+                int tsp = nums[i];
+                
+                while(map.containsKey(tsp+tl)){
+                    tl++;
+                }
+                
+                if(tl>maxLen)
+                    maxLen = tl;
+                
+            }
+        }
+        
+        return maxLen;
     }
 }
