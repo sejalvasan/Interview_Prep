979be7fd0400d0af67b3c97fc14d3f42a8f6a9d8
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-07 19:04:07 +0530

    Time: 8 ms (62.42%), Space: 47 MB (53.78%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,49 @@
+class Pair{
+    int src;
+    int wt;
+    Pair(int src, int wt){
+        this.src = src;
+        this.wt = wt;
+    }
+
+}
+ 
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Queue<Pair> q = new LinkedList<>();
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++)
+            adj.add(new ArrayList<>());
+        
+        
+        for(int[]row: flights){
+            int u = row[0];
+            int v = row[1];
+            int w = row[2];
+            adj.get(u).add(new Pair(v,w));
+        }
+        q.add(new Pair(src,0));
+        int min = Integer.MAX_VALUE;
+        int level=0;
+
+        int[]dis = new int[n];
+        dis[src]=0;
+        
+        Arrays.fill(dis,Integer.MAX_VALUE);
+        
+        while(!q.isEmpty() && level<=k){
+          int size = q.size();
+            while(size-->0){
+                Pair rem = q.remove();
+                for(Pair nbr: adj.get(rem.src)){
+                    if(rem.wt+nbr.wt<dis[nbr.src]){
+                        dis[nbr.src]= rem.wt + nbr.wt;
+                        q.add(new Pair(nbr.src, dis[nbr.src]));
+                    }
+                }
+            }
+            level++;
+        }
+        return dis[dst]==Integer.MAX_VALUE?-1:dis[dst];
+    }
+}
