ea189ddcecc2e407f4259db69da976297e1aaa0f
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-09 18:21:02 +0530

    Time: 52 ms (35.44%), Space: 140.4 MB (44.51%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
@@ -1,80 +1,84 @@
 class Solution {
-    private int find(int x, int [] parent){
-        if(parent[x] == x){
-            return x;
-        }
-        int sl = find(parent[x], parent);
-        parent[x] = sl;
-        return sl;
-    }
-    private boolean union(int x, int y, int [] parent, int [] rank){
-        int slx = find(x, parent);
-        int sly = find(y, parent);
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        int remove =0;
         
-        if(slx != sly){
-            if(rank[slx] < rank[sly]){
-                parent[slx] = sly;
-            } else if(rank[slx] > rank[sly]){
-                parent[sly] = slx;
-            } else {
-                parent[sly] = slx;
-                rank[slx]++;
+        Arrays.sort(edges,(a,b)->b[0]-a[0]);
+        
+        int[]parenta = new int[n+1];
+        int[]parentb = new int[n+1];
+        int[]ranka = new int[n+1];
+        int[]rankb = new int[n+1];
+        
+        for(int i=1;i<n+1;i++){
+            parenta[i]=i;
+            parentb[i]=i;
+            ranka[i]=1;
+            rankb[i]=1;
+        }
+        
+        int mergeda =1;
+        int mergedb =1;
+        
+        for(int[]e:edges){
+            if(e[0]==3){
+                boolean flaga = union(e[1],e[2],parenta, ranka);
+                boolean flagb = union(e[1],e[2],parentb, rankb);
+                
+                if(flaga==true)
+                    mergeda++;
+                
+                if(flagb==true)
+                    mergedb++;
+                
+                if(flaga==false && flagb==false)
+                    remove++;
+                
+            }else if(e[0]==1){
+                 boolean flaga = union(e[1],e[2],parenta, ranka);
+                  if(flaga==true)
+                    mergeda++;
+                else
+                    remove++;
+            }else {
+                    boolean flagb = union(e[1],e[2],parentb, rankb);
+                    if(flagb==true)
+                    mergedb++;
+                   else
+                    remove++;
+            }
+        }
+        
+        if(mergeda!=n || mergedb!=n)
+            return -1;
+        
+        return remove;
+        
+    }
+    
+    public boolean union(int x, int y, int[]parent, int[]rank){
+        int lx = find(x,parent);
+        int ly = find(y,parent);
+        
+        if(lx!=ly){
+            if(rank[lx]<rank[ly]){
+                 parent[ly]=lx;
+            }else if(rank[lx]>rank[ly]){
+                parent[ly]=lx;
+            }else {
+                parent[ly]=lx;
+                lx++;
             }
             return true;
         }
         return false;
     }
-    public int maxNumEdgesToRemove(int n, int[][] edges) {
-        Arrays.sort(edges, (a, b) -> Integer.compare(b[0], a[0]));
-        int [] parenta = new int[n + 1];
-        int [] parentb = new int[n + 1];
-        int [] ranka = new int[n + 1];
-        int [] rankb = new int[n + 1];
+    
+    public int find(int x,int[]parent){
+        if(parent[x]==x)
+            return x;
         
-        for(int i = 0; i <= n; i++){
-            parenta[i] = i;
-            parentb[i] = i;
-            ranka[i] = 1;
-            rankb[i] = 1;
-        }
-        
-        int mergeda = 1;
-        int mergedb = 1;
-        
-        int remove = 0;
-        for(int [] e : edges){
-            if(e[0] == 3){
-                boolean tempa = union(e[1], e[2], parenta, ranka);
-                boolean tempb = union(e[1], e[2], parentb, rankb);
-                
-                if(tempa == true){
-                    mergeda++;
-                } 
-                if(tempb == true){
-                    mergedb++;
-                }
-                if(tempa == false && tempb == false){
-                    remove++;
-                }
-            } else if(e[0] == 1){
-                boolean tempa = union(e[1], e[2], parenta, ranka);
-                if(tempa == true){
-                    mergeda++;
-                } else {
-                    remove++;
-                }
-            } else {
-                boolean tempb = union(e[1], e[2], parentb, rankb);
-                if(tempb == true){
-                    mergedb++;
-                } else {
-                    remove++;
-                }
-            }
-        }
-        if(mergeda != n || mergedb != n){
-            return -1;
-        }
-        return remove;
+        int sl = find(parent[x],parent);
+        parent[x] = sl;
+        return sl;
     }
 }
