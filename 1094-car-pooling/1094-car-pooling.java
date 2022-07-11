e8944f5f72ba366f89b671d0457dd26e17a01b91
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-11 15:34:35 +0530

    Time: 2 ms (87.65%), Space: 44.1 MB (77.50%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        
+         int lastDrop=0;
+        
+        for(int[] trip : trips){
+            lastDrop=Math.max(lastDrop,trip[2]);
+        }
+        
+        int highway[]=new int[lastDrop+1];
+        for(int trip[] : trips){
+            
+            highway[trip[1]] +=trip[0];
+            highway[trip[2]] -=trip[0];
+        
+        }
+        
+        if(highway[0]>capacity) return false;
+        
+        for(int i=1;i<=lastDrop;i++){
+         
+            highway[i] +=highway[i-1];
+            if(highway[i]>capacity){
+                return false;
+            }
+            
+        }
+        return true;
+        
+        
+    }
+}
