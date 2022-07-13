3d58a14fa3474e688c20973e03f00d77718321d1
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-13 17:43:21 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..java
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s1 = sc.next();
+                    String s2 = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minOperations(s1,s2));
+                }
+        }
+}    // } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+	public int minOperations(String s1, String s2) 
+	{ 
+	    // Your code goes here
+	    
+	    char []X =s1.toCharArray();
+	    char []Y = s2.toCharArray();
+	    int m = s1.length();
+	    int n = s2.length();
+	    
+	    int[][]dp = new int[m+1][n+1];
+	    
+	    for(int i=1;i<=m;i++){
+	        for(int j =1;j<=n;j++){
+	            
+	            if(X[i-1]==Y[j-1])
+	           dp[i][j]=1+dp[i-1][j-1];
+	           
+	           else
+	           dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+	        }
+	    }
+	    
+	    int lcs = dp[m][n];
+	    int del = m-lcs;
+	    int inst = n-lcs;
+	    
+	    return del+inst;
+	} 
+}
