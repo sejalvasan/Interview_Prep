a970f7ce794523e29316e7cc880d2caad73295c6
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-13 12:17:30 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.java
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.java
@@ -50,24 +50,15 @@ class Solution{
     public static boolean targetsumsubet(int n,int[]arr,int target){
         
          boolean dp[][]=new boolean[n+1][target+1];
+         
        for (int i = 0; i <n + 1; i++) {
-      for (int j = 0; j < target + 1; j++) {
-        if (i == 0) {
-          dp[i][j] = false;
-        }
-        if (j == 0) {
-          dp[i][j] = true;
+        for (int j = 0; j < target + 1; j++) {
+            if (j == 0) 
+             dp[i][j] = true;
         }
       }
-    }
         for(int i=1;i<n+1;i++){
             for(int j=1;j<target+1;j++){
-                
-                if(i==0)
-                dp[i][j]=false;
-                
-                if(j==0)
-                dp[i][j]=true;
                 
                 if(arr[i-1]<=j)
                 dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
