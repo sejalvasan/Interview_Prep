8b306e40013dccf33b468f7cbab2e2ffc305d6a3
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-13 16:31:13 +0530

    Added solution - LeetHub

---
+++ Longest Common Substring - GFG/longest-common-substring.java
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(input[0]);
+            int m = Integer.parseInt(input[1]);
+            
+            String S1 = read.readLine().trim();
+            String S2 = read.readLine().trim();
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestCommonSubstr(S1, S2, n, m));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int longestCommonSubstr(String s1, String s2, int m, int n){
+        // code here
+    char[] X=s1.toCharArray();
+    char[] Y=s2.toCharArray();
+    
+      int L[][] = new int[m+1][n+1];
+      int max = 0;
+ 
+    /* Following steps build L[m+1][n+1] in bottom up fashion. Note
+        that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
+    for (int i=0; i<=m; i++)
+    {
+    for (int j=0; j<=n; j++)
+    {
+        if (i == 0 || j == 0)
+            L[i][j] = 0;
+        else if (X[i-1] == Y[j-1])
+            L[i][j] = L[i-1][j-1] + 1;
+            
+            if(max<L[i][j])
+            max = L[i][j];
+     }
+    }
+   return max;
+ }
+}
