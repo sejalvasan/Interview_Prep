f8dc5a63c7fd4595ab47e54cc64273c8c6e1943f
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-18 23:24:05 +0530

    Time: 5 ms (88.21%), Space: 44 MB (71.76%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,18 +1,55 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[]lis = new int[nums.length];
+//         int[]lis = new int[nums.length];
         
-        Arrays.fill(lis,1);
-        int max = 1;
-        for(int i =0;i<nums.length;i++){
-            for(int j =0;j<i;j++){
-                if(nums[i]>nums[j] && lis[i]<lis[j]+1)
-                    lis[i]=lis[i]+1;
+//         Arrays.fill(lis,1);
+//         int max = 1;
+//         for(int i =0;i<nums.length;i++){
+//             for(int j =0;j<i;j++){
+//                 if(nums[i]>nums[j] && lis[i]<lis[j]+1)
+//                     lis[i]=lis[i]+1;
                 
-                max = Math.max(max,lis[i]);
-            }
+//                 max = Math.max(max,lis[i]);
+//             }
+//         }
+        
+//         return max;
+//     }
+// }
+        int n = nums.length;
+        int[]tail = new int[n];
+        tail[0]=nums[0];
+        
+        int lastIndex =0;
+        
+        for(int i =1;i<n;i++){
+            int index = find(tail,0,lastIndex,nums[i]);
+            
+            tail[index]=nums[i];
+            
+            lastIndex = Math.max(index,lastIndex);
         }
         
-        return max;
+        return lastIndex+1;
+    }
+    
+    public int find(int[]nums, int l, int h, int val){
+        
+        if(val>nums[h])
+            return h+1;
+        
+        while(l<h){
+            int mid = (h+l)/2;
+            
+            if(nums[mid]==val)
+                return mid;
+                
+             else if(nums[mid]<val)
+                 l = mid+1;
+            else
+                h = mid;
+        }
+        
+        return h;
     }
 }
