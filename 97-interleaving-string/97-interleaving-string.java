38d9e70a0fc8b2f9e224dc4da2c8e52be965249d
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-19 12:56:38 +0530

    Time: 2 ms (85.17%), Space: 40.2 MB (98.60%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-         int len1 = s1.length();
+        
+        int len1 = s1.length();
         int len2 = s2.length();
         int len3 = s3.length();
         
@@ -11,7 +12,7 @@ class Solution {
         dp[0][0] = true;
 
         for (int i = 1; i < dp[0].length; i++){
-            dp[0][i] = dp[0][i-1]&&(s1.charAt(i-1)==s3.charAt(i-1));
+            dp[0][i] = dp[0][i-1] && (s1.charAt(i-1)==s3.charAt(i-1));
         }
 
         for (int i = 1; i < dp.length; i++){
@@ -20,8 +21,8 @@ class Solution {
 
         for (int i = 1; i < dp.length; i++){
             for (int j = 1; j < dp[0].length; j++){
-                dp[i][j] = (dp[i-1][j]&&(s2.charAt(i-1)==s3.charAt(i+j-1)))
-                        || (dp[i][j-1]&&(s1.charAt(j-1)==s3.charAt(i+j-1)));
+                dp[i][j] = (dp[i-1][j] && (s2.charAt(i-1)==s3.charAt(i+j-1)))
+                        || (dp[i][j-1] && (s1.charAt(j-1)==s3.charAt(i+j-1)));
             }
         }
 
