71097d5990eb4e83281f50e0f909c1297b3f668a
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-19 14:56:48 +0530

    Time: 5 ms (55.87%), Space: 42.1 MB (67.77%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,31 +1,36 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         
-        int len1 = s1.length();
-        int len2 = s2.length();
-        int len3 = s3.length();
+      if(s1.length()+s2.length()!=s3.length())
+         return false;
+     Boolean dp[][]=new Boolean[s1.length()+1][s2.length()+1];
+     return solve(s1,s2,s3,0,0,dp);
+    }
+    
+    Boolean solve(String s1, String s2, String s3,int i,int j,Boolean dp[][])
+    {
+     
+     if(i==s1.length()&&j==s2.length())
+         return true;//perfectly matched if got till here
         
-        if ((len1+len2)!=len3) return false;
-
-        boolean[][] dp = new boolean[len2+1][len1+1];
-
-        dp[0][0] = true;
-
-        for (int i = 1; i < dp[0].length; i++){
-            dp[0][i] = dp[0][i-1] && (s1.charAt(i-1)==s3.charAt(i-1));
-        }
-
-        for (int i = 1; i < dp.length; i++){
-            dp[i][0] = dp[i-1][0]&&(s2.charAt(i-1)==s3.charAt(i-1));
-        }
-
-        for (int i = 1; i < dp.length; i++){
-            for (int j = 1; j < dp[0].length; j++){
-                dp[i][j] = (dp[i-1][j] && (s2.charAt(i-1)==s3.charAt(i+j-1)))
-                        || (dp[i][j-1] && (s1.charAt(j-1)==s3.charAt(i+j-1)));
-            }
-        }
-
-        return dp[len2][len1];
+     if(dp[i][j]!=null)
+         return dp[i][j];
+        
+     if(i<s1.length()&&s1.charAt(i)==s3.charAt(i+j))
+     {
+         dp[i][j]=solve(s1,s2,s3,i+1,j,dp);
+         if(dp[i][j]==true)//directly return mat krna ,next ko bhi check 
+            return dp[i][j];
+     }
+        
+     if(j<s2.length()&&s2.charAt(j)==s3.charAt(i+j))
+     {
+         dp[i][j]=solve(s1,s2,s3,i,j+1,dp);
+         if(dp[i][j]==true)
+             return dp[i][j];
+     }
+     
+     dp[i][j]=false;
+     return dp[i][j];
     }
 }
