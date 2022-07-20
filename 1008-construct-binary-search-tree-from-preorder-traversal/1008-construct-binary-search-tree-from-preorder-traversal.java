0e114b59c4028116769df032d9e25bb273318a19
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-20 13:50:16 +0530

    Time: 1 ms (66.05%), Space: 42.5 MB (28.31%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,27 +14,25 @@
  * }
  */
 class Solution {
-    int index=0;
     public TreeNode bstFromPreorder(int[] preorder) {
-        return construct(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+      return construct(preorder,Integer.MIN_VALUE,Integer.MAX_VALUE);
     }
-    
-    public TreeNode construct(int[]pre,int min,int max){
-        
-        if(index==pre.length){
-            return null;
-        }else if(pre[index]>min && pre[index]<max){
+    int index =0;
+    public TreeNode construct(int[]preorder, int min, int max){
+        if(index == preorder.length){
+         return null;   
+        }else if(preorder[index]>min && preorder[index]<max){
             TreeNode node = new TreeNode();
-            node.val = pre[index];
+            node.val = preorder[index];
             
             index++;
-            node.left = construct(pre,min,node.val);
-            
-            node.right = construct(pre, node.val,max);
+            node.left = construct(preorder, min, node.val);
+            node.right = construct(preorder,node.val,max);
             
             return node;
         }else{
             return null;
         }
+
     }
 }
