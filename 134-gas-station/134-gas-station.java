af5ccca63d87a7c83016660b266de0de94a11730
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-25 13:30:43 +0530

    Time: 2 ms (95.14%), Space: 84.8 MB (5.72%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,7 +1,46 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         
-        int n = gas.length;
+//        int n = gas.length;
+
+//         int totgas = 0;
+//         int totcost = 0;
+//         for(int i =0;i<n;i++){
+//             totgas+=gas[i];
+//             totcost+=cost[i];
+//         }       
+        
+//         if(totcost>totgas)
+//             return -1;
+        
+//         int [] delta = new int[n];
+//         for(int i=0;i<n;i++){
+//             delta[i]=gas[i]-cost[i];
+//         }
+        
+//         int[]deltasum = new int[n];
+//         deltasum[0]=delta[0];
+//         for(int i =1;i<n;i++){
+//             deltasum[i]=deltasum[i-1]+delta[i];
+//         }
+        
+//         int idx=0;
+//         int min =Integer.MAX_VALUE;
+//         for(int i=0;i<n;i++){
+//             if(deltasum[i]<min){
+//                 min=deltasum[i];
+//                 idx=i;
+//             }
+//         }
+        
+//         if(idx!=n-1)
+//         return idx+1;
+        
+//         return 0;
+//     }
+// }
+        
+          int n = gas.length;
 
         int totgas = 0;
         int totcost = 0;
@@ -13,28 +52,17 @@ class Solution {
         if(totcost>totgas)
             return -1;
         
-        int [] delta = new int[n];
-        for(int i=0;i<n;i++){
-            delta[i]=gas[i]-cost[i];
-        }
+        int remainingGas =0;
+        int start =0;
         
-        int[]deltasum = new int[n];
-        deltasum[0]=delta[0];
-        for(int i =1;i<n;i++){
-            deltasum[i]=deltasum[i-1]+delta[i];
-        }
-        
-        int idx=0;
-        int min =Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
-            if(deltasum[i]<min){
-                min=deltasum[i];
-                idx=i;
+            remainingGas += gas[i]-cost[i];
+            if(remainingGas<0){
+             start=i+1;
+                remainingGas =0;
             }
         }
-        if(idx!=n-1)
-        return idx+1;
         
-        return 0;
+        return start;
     }
 }
