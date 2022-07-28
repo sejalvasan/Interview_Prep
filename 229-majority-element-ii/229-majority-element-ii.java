0ee17b11638ff469e44bbd4f195efd098d0cef22
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-28 13:57:12 +0530

    Time: 2 ms (93.51%), Space: 50.1 MB (34.99%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,44 +1,44 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int val1=nums[0];
+        
+        int val1 = nums[0];
         int count1 =1;
-        int val2 = -1;
-        int count2=0;
+        int val2 =0;
+        int count2 =0;
         
         for(int i =1;i<nums.length;i++){
-            if(val1==nums[i])
+            if(nums[i]==val1){
                 count1++;
-        else if(val2==nums[i])
-            count2++;
-        else if(count1==0){
-            val1=nums[i];
-            count1++;
-        }else if(count2==0){
-            val2=nums[i];
-            count2++;
-        }else{
-            count1--;
-            count2--;
-        }
+            }else if(nums[i]==val2)
+                count2++;
+            else if(count1==0){
+                val1 = nums[i];
+                count1++;
+            }else if(count2==0){
+                val2=nums[i];
+                count2++;
+            }else{
+                count1--;
+                count2--;
+            }
         }
         
-        //check for occurences
         count1=0;
         count2=0;
+        
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==val1)
-                count1++;
+          if(nums[i]==val1)
+              count1++;
             else if(nums[i]==val2)
                 count2++;
         }
-        
-        ArrayList<Integer> arr = new ArrayList<>();
+                ArrayList<Integer> res = new ArrayList<>();
+
         if(count1>nums.length/3)
-            arr.add(val1);
-        
-           if(count2>nums.length/3)
-            arr.add(val2);
-        
-        return arr;
+        res.add(val1);
+                if(count2>nums.length/3)
+
+        res.add(val2);
+        return res;
     }
 }
