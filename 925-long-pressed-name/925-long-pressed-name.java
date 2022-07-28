929fed2a700fd9dca4ef3ccffb858191ff6cf499
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-07-28 13:33:25 +0530

    Time: 1 ms (91.83%), Space: 42.1 MB (49.54%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.java
+++ 925-long-pressed-name/925-long-pressed-name.java
@@ -1,32 +1,34 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
+        
         if(name.length()>typed.length())
             return false;
         
-        int i = 0;
-        int j =0;
+        int i =0, j=0;
         
         while(i<name.length() && j<typed.length()){
-            if(name.charAt(i)==typed.charAt(j))
-            {
+            //letters are equal
+            if(name.charAt(i)==typed.charAt(j)){
                 i++;
                 j++;
-            }else if(i>0 && typed.charAt(j)==name.charAt(i-1)){
-                j++;
             }
-        else{
-                    return false; //eg aac and bc
-                }
-        }
-    //if j is left
-        while(j<typed.length()){
-            if(name.charAt(i-1)!=typed.charAt(j))
+            else if(i>0 && typed.charAt(j)==name.charAt(i-1)){
+                j++;
+            }else{
                 return false;
-            j++;
+            }
         }
         
-        //if i is left
+        //j is left
         
-        return i<name.length()?false: true;
+       while(j<typed.length()){
+           if(typed.charAt(j)==name.charAt(i-1))
+               j++;
+           
+           else
+               return false;
+       }
+         
+        return i==name.length()?true:false;
     }
 }
