d870370428a486ffc41e053d70427208464bf4af
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-08-01 19:04:25 +0530

    Time: 7 ms (70.12%), Space: 47.6 MB (36.16%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -10,8 +10,11 @@ class Pair{
  
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        
         Queue<Pair> q = new LinkedList<>();
+        
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        
         for(int i=0;i<n;i++)
             adj.add(new ArrayList<>());
         
@@ -22,28 +25,27 @@ class Solution {
             int w = row[2];
             adj.get(u).add(new Pair(v,w));
         }
-        q.add(new Pair(src,0));
-        int min = Integer.MAX_VALUE;
-        int level=0;
-
-        int[]dis = new int[n];
         
-        Arrays.fill(dis,Integer.MAX_VALUE);
+        q.add(new Pair(src,0));
+        int[]dis = new int[n];
+        Arrays.fill(dis, Integer.MAX_VALUE);
         dis[src]=0;
+        int level =0;
         
         while(!q.isEmpty() && level<=k){
-          int size = q.size();
+            int size = q.size();
             while(size-->0){
-                Pair rem = q.remove();
-                for(Pair nbr: adj.get(rem.src)){
+            Pair rem = q.remove();
+                for(Pair nbr:adj.get(rem.src)){
                     if(rem.wt+nbr.wt<dis[nbr.src]){
-                        dis[nbr.src]= rem.wt + nbr.wt;
-                        q.add(new Pair(nbr.src, dis[nbr.src]));
+                        dis[nbr.src]=rem.wt+nbr.wt;
+                        q.add(new Pair(nbr.src,dis[nbr.src]));
                     }
                 }
             }
             level++;
         }
-        return dis[dst]==Integer.MAX_VALUE?-1:dis[dst];
+    return dis[dst]==Integer.MAX_VALUE?-1:dis[dst];
+
     }
 }
