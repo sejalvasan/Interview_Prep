5e3e270c8d7206ff093749fe66522c7861e96294
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-08-02 16:27:37 +0530

    Time: 1 ms (88.87%), Space: 42.3 MB (42.97%) - LeetHub

---
+++ 394-decode-string/394-decode-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String decodeString(String s) {
+        
+        Stack<StringBuilder> strStack= new Stack<>();
+        Stack<Integer> freqStack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int k =0;
+        
+        for(char c:s.toCharArray()){
+              if(Character.isDigit(c)){
+                  k=k*10+(c-'0'); // if k is of multiple digits
+              }else if(Character.isLetter(c)){
+                  sb.append(c);
+              }else if(c=='['){
+                  strStack.push(sb);
+                  freqStack.push(k);
+                  //reset
+                  k=0;
+                  sb = new StringBuilder();
+              }else{
+                  // ]
+                  StringBuilder temp = sb;
+                  int freq = freqStack.pop();
+                  sb = strStack.pop();
+                  
+                  while(freq>0){
+                      sb.append(temp);
+                      freq--;
+                  }
+                  k=0;
+              }
+        }
+        return sb.toString();
+    }
+}
