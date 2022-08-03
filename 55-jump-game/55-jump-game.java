72f94d10ee90a06dc2f1cba20eb8cc02c4d0e284
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-08-03 14:01:14 +0530

    Time: 1 ms (100.00%), Space: 68.3 MB (31.47%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+        int index = nums.length-1;
+        
+        for(int i = nums.length-1;i>=0;i--){
+            if(nums[i]+i>=index)
+                index = i;
+        }
+        
+        if(index==0)
+            return true;
+        
+        return false;
+    }
+}
