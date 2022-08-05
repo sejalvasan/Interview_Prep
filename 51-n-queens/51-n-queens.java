d297f646a71fe6cc01633eb7bce55a85cb05a363
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-08-05 13:17:44 +0530

    Time: 2 ms (97.82%), Space: 45.5 MB (51.08%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,45 +1,53 @@
 class Solution {
+    List<List<String>> res = new ArrayList<>();
+    
     public List<List<String>> solveNQueens(int n) {
-        char[][] board = new char[n][n];
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < n; j++)
-                board[i][j] = '.';
-        List<List<String>> res = new ArrayList<List<String>>();
-        int leftRow[] = new int[n];
-        int upperDiagonal[] = new int[2*n-1]; 
-        int lowerDiagonal[] = new int[2*n-1]; 
-        solve(0, board, res, leftRow, lowerDiagonal, upperDiagonal);
+        
+        char[][]board = new char[n][n];
+        
+        for(int i =0;i<n;i++){
+            for(int j =0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        
+        int[]left = new int[n];
+        int down[] = new int[2*n-1];
+        int up[] = new int[2*n-1];
+
+        solve(0,n,board,left,down,up);
         return res;
     }
     
-    private void solve(int col, char[][] board, List<List<String>> res, int leftRow[], int lowerDiagonal[], int upperDiagonal[]) {
-        if(col == board.length) {
+    public void solve(int col, int n, char[][]board, int[]left, int down[], int[]up){
+        
+        if(col==n){
             res.add(construct(board));
             return;
         }
         
-        for(int row = 0; row < board.length; row++) {
-            if(leftRow[row] == 0 && lowerDiagonal[row+col] == 0 && upperDiagonal[board.length -1 + col - row] == 0) {
-                board[row][col] = 'Q';
-                leftRow[row] = 1;
-                lowerDiagonal[row+col] = 1;
-                upperDiagonal[board.length-1 + col - row] = 1;
-                solve(col+1, board, res, leftRow, lowerDiagonal, upperDiagonal );
-                board[row][col] = '.';
-                leftRow[row] = 0;
-                lowerDiagonal[row+col] = 0;
-                upperDiagonal[board.length - 1 + col - row] = 0;
+        for(int row=0;row<n;row++){
+            if(left[row]==0 && down[row+col]==0 && up[n-1+col-row]==0){
+                board[row][col]='Q';
+                left[row]=1;
+                up[n-1+col-row]=1;
+                down[row+col]=1;
+                solve(col+1,n,board,left,down,up);
+                 board[row][col]='.';
+                left[row]=0;
+                up[n-1+col-row]=0;
+                down[row+col]=0;
             }
         }
     }
     
-    
-    private List<String> construct(char[][] board) {
-        List<String> res = new ArrayList<String>();
-        for(int i = 0; i < board.length; i++) {
+    public List<String> construct(char[][]board){
+        List<String> ans = new ArrayList<>();
+        
+        for(int i =0;i<board.length;i++){
             String s = new String(board[i]);
-            res.add(s);
+            ans.add(s);
         }
-        return res;
+        return ans;
     }
 }
