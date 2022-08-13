672a2b6dd830827389ce8dbf8db1c57fb2f9bd30
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-08-14 03:13:40 +0530

    Time: 12 ms (79.93%), Space: 43.8 MB (99.71%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,40 +1,43 @@
 class Pair{
-    int first;
-    int second;
-    
-    Pair(int first, int second){
-        this.first =first;
-        this.second =second;
+    int x;
+    int y;
+    Pair(int x, int y){
+        this.x=x;
+        this.y =y;
     }
 }
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-     int n = mat.length;
-        int m =mat[0].length;
-        Queue<Pair> que = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        
+        Queue<Pair> q = new LinkedList<>();
+      
+        for(int i =0;i<mat.length;i++){
+            for(int j =0;j<mat[0].length;j++){
                 if(mat[i][j]==0)
-                    que.add(new Pair(i,j));
-            else
-                mat[i][j]=-1;
+                    q.add(new Pair(i,j));
+                else
+                    mat[i][j]=-1;
             }
         }
         
-        int dir[][]={{0,1},{1,0},{-1,0},{0,-1}};
+        int[][]dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
         
-       while(!que.isEmpty()){
-           Pair rem = que.remove();
-           for(int i=0;i<4;i++){
-               int rdash = rem.first + dir[i][0];
-               int cdash = rem.second + dir[i][1];
-               
-               if(rdash>=0 && cdash>=0 && rdash <n && cdash<m && mat[rdash][cdash]<0){
-                   que.add(new Pair(rdash,cdash));
-                   mat[rdash][cdash]=mat[rem.first][rem.second]+1;
-               }
-           }
-       }
+        while(!q.isEmpty()){
+            Pair rem = q.remove();
+            
+            for(int i =0;i<4;i++){
+                int r = rem.x + dirs[i][0];
+                int c = rem.y + dirs[i][1];
+                
+                 if(r>=0 && c>=0 && r <mat.length && c<mat[0].length && mat[r][c]<0){
+                
+                mat[r][c]=mat[rem.x][rem.y]+1;
+                
+                q.add(new Pair(r,c));
+                 }
+            }
+            
+        }
         return mat;
     }
 }
