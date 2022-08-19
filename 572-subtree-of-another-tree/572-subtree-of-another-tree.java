fc8589dad7407c05fd6031ce0c0f9e32c774165b
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-08-19 10:17:45 +0530

    Time: 6 ms (20.25%), Space: 47.7 MB (5.05%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -16,20 +16,42 @@
 class Solution {
     //public boolean isSubtree(TreeNode s, TreeNode subRoot) {
    public boolean isSubtree(TreeNode s, TreeNode t) {
-StringBuilder sbs = new StringBuilder(",");
-preOrder(s, sbs);
-StringBuilder sbt = new StringBuilder(",");
-preOrder(t, sbt);
+       
+  StringBuilder sbs = new StringBuilder();
+    preOrder(s, sbs);
+    StringBuilder sbt = new StringBuilder();
+    preOrder(t, sbt);
+  
 
-    return sbs.toString().contains(sbt.toString());
+        if(sbs.toString().contains(sbt.toString())==false)
+        return false;
+       
+        StringBuilder sss = new StringBuilder();
+        inOrder(s, sss);
+        StringBuilder sbb = new StringBuilder();
+        inOrder(t, sbb);
+       
+      // inOrder(t, sbt);
+         if(sss.toString().contains(sbb.toString())==false)
+        return false;
+      //return sss.toString().contains(sbb.toString());
+       return true;
 }
 
-public StringBuilder preOrder(TreeNode root, StringBuilder sb){
-    if(root==null)
-        return sb.append("#,");
-    sb.append(root.val).append(",");
-    preOrder(root.left, sb);
-    preOrder(root.right, sb);
-    return sb;
-}
+    public static StringBuilder preOrder(TreeNode root, StringBuilder sb){
+        if(root==null)
+            return sb.append("#");
+        sb.append(root.val);
+        preOrder(root.left, sb);
+        preOrder(root.right, sb);
+        return sb;
     }
+    
+    public static StringBuilder inOrder(TreeNode root, StringBuilder sb){
+    if(root==null)
+        return sb.append("#");
+    inOrder(root.left, sb);
+    sb.append(root.val);
+    inOrder(root.right, sb);
+    return sb;
+}}
