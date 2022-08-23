f5b69d0e11af26ee730a9bcac428584e4ab74a9f
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-08-23 23:02:06 +0530

    Time: 10 ms (55.35%), Space: 46.3 MB (59.67%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+     Arrays.sort(logs,(log1,log2)->{
+         int index1 = log1.indexOf(" ");
+         String id1 = log1.substring(0,index1);
+         String main1 = log1.substring(index1+1);
+         
+         int index2 = log2.indexOf(" ");
+         String id2 = log2.substring(0,index2);
+         String main2 = log2.substring(index2+1);
+         
+         boolean isDigit1 = Character.isDigit(main1.charAt(0));
+         boolean isDigit2 = Character.isDigit(main2.charAt(0));
+         
+         if(!isDigit1 && !isDigit2){
+             int value = main1.compareTo(main2);
+             if(value == 0) return id1.compareTo(id2);
+             return value;
+         }
+         
+         //nor chars --> digits
+         // 1 because it means log2 is a letter log and letter logs come before digit logs
+         return isDigit1? ( isDigit2? 0: 1):-1;
+     });
+         return logs;
+    }
+}
