c334021286ebb21edf8e1777b7b3370e066d7ac8
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-08-23 23:36:30 +0530

    Time: 35 ms (69.44%), Space: 58.6 MB (78.79%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,10 +1,13 @@
 class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        
         List<List<String>> ans = new ArrayList();
         Arrays.sort(products);
+        
         for(int i=0;i<searchWord.length();++i) {
             List<String> ians = new ArrayList();
             String subs = searchWord.substring(0,i+1); 
+            
             int idx = binarySearch(products, subs);
             for(int j=idx;j<products.length;++j) {
                 if(products[j].startsWith(subs)) {
@@ -27,6 +30,6 @@ class Solution {
             else 
                 left=mid+1;
         }
-        return right+1; 
+        return left; 
     }
 }
