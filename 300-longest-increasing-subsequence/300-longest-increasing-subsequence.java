8ce4b1d1cf739b475de4ab785b79aeee4aa31192
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-09-14 17:27:16 +0530

    Time: 71 ms (55.79%), Space: 41.7 MB (95.38%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,55 +1,56 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-//         int[]lis = new int[nums.length];
-        
-//         Arrays.fill(lis,1);
-//         int max = 1;
-//         for(int i =0;i<nums.length;i++){
-//             for(int j =0;j<i;j++){
-//                 if(nums[i]>nums[j] && lis[i]<lis[j]+1)
-//                     lis[i]=lis[i]+1;
-                
-//                 max = Math.max(max,lis[i]);
-//             }
-//         }
-        
-//         return max;
-//     }
-// }
-        int n = nums.length;
-        int[]tail = new int[n];
-        tail[0]=nums[0];
-        
-        int lastIndex =0;
-        
-        for(int i =1;i<n;i++){
-            int index = find(tail,0,lastIndex,nums[i]);
-            
-            tail[index]=nums[i];
-            
-            lastIndex = Math.max(index,lastIndex);
+        int[]lis = new int[nums.length];
+        int omax =0;
+        for(int i =0;i<nums.length;i++){
+            int max = 0;
+            for(int j =0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    if(lis[j]>max)
+                        max = lis[j];
+                }
+            }
+            lis[i]=max+1;
+        omax = Math.max(omax,lis[i]);
         }
         
-        return lastIndex+1;
-    }
-    
-    public int find(int[]nums, int l, int h, int val){
-        
-        if(val>nums[h])
-            return h+1;
-        
-        while(l<h){
-            int mid = (h+l)/2;
-            
-            if(nums[mid]==val)
-                return mid;
-                
-             else if(nums[mid]<val)
-                 l = mid+1;
-            else
-                h = mid;
-        }
-        
-        return h;
+        return omax;
     }
 }
+//         int n = nums.length;
+//         int[]tail = new int[n];
+//         tail[0]=nums[0];
+        
+//         int lastIndex =0;
+        
+//         for(int i =1;i<n;i++){
+//             int index = find(tail,0,lastIndex,nums[i]);
+            
+//             tail[index]=nums[i];
+            
+//             lastIndex = Math.max(index,lastIndex);
+//         }
+        
+//         return lastIndex+1;
+//     }
+    
+//     public int find(int[]nums, int l, int h, int val){
+        
+//         if(val>nums[h])
+//             return h+1;
+        
+//         while(l<h){
+//             int mid = (h+l)/2;
+            
+//             if(nums[mid]==val)
+//                 return mid;
+                
+//              else if(nums[mid]<val)
+//                  l = mid+1;
+//             else
+//                 h = mid;
+//         }
+        
+//         return h;
+//     }
+// }
