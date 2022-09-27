862c1eda14c9b072b0ffbc6efcaa5b84bdf921ee
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-09-27 17:13:47 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (92.12%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,24 +1,36 @@
 class Solution{
 public int uniquePaths(int m, int n) {
-        int[][]dp = new int[m][n];
+//         int[][]dp = new int[m][n];
         
-        for(int[]row:dp)
-        Arrays.fill(row,-1);
+//         for(int[]row:dp)
+//         Arrays.fill(row,-1);
         
-     return countPaths(m,n,0,0,dp);
+//      return countPaths(m,n,0,0,dp);
+//     }
+    
+//         public int countPaths(int m, int n, int i, int j, int[][]dp){
+//             //end point
+//         if(i==m-1 && j==n-1)
+//             return 1;
+        
+//         if(i>=m || j>=n)
+//             return 0;
+            
+//             if(dp[i][j]!=-1)
+//                 return dp[i][j];
+        
+//             else    
+//         return dp[i][j]=countPaths(m,n,i+1,j,dp)+countPaths(m,n,i,j+1,dp);
+    
+    int N = m+n-2;
+    int r = m-1;
+    double res = 1;
+    
+    for(int i =1;i<=r;i++){
+        res = res *(N-r+i)/i;
     }
     
-        public int countPaths(int m, int n, int i, int j, int[][]dp){
-            //end point
-        if(i==m-1 && j==n-1)
-            return 1;
-        
-        if(i>=m || j>=n)
-            return 0;
-            
-            if(dp[i][j]!=-1)
-                return dp[i][j];
-                else    
-        return dp[i][j]=countPaths(m,n,i+1,j,dp)+countPaths(m,n,i,j+1,dp);
+    return (int)res;
     }
+    
 }
