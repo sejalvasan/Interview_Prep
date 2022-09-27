00cf4b285aa60fc44ec5778a334a8f713a778e02
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-09-27 18:20:39 +0530

    Time: 1 ms (56.38%), Space: 40.8 MB (66.56%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -22,15 +22,35 @@ public int uniquePaths(int m, int n) {
 //             else    
 //         return dp[i][j]=countPaths(m,n,i+1,j,dp)+countPaths(m,n,i,j+1,dp);
     
-    int N = m+n-2;
-    int r = m-1;
-    double res = 1;
+//     int N = m+n-2;
+//     int r = m-1;
+//     double res = 1;
     
-    for(int i =1;i<=r;i++){
-        res = res *(N-r+i)/i;
+//     for(int i =1;i<=r;i++){
+//         res = res *(N-r+i)/i;
+//     }
+    
+//     return (int)res;
+    
+    int[][]dp = new int[m][n];
+    
+    for(int i =0;i<m;i++){
+        for(int j =0;j<n;j++){
+            if(i==0 && j==0)
+                dp[i][j]=1;
+            else{
+                int up=0;
+                int left = 0;
+                if(i>0)
+                    up = dp[i-1][j];
+                if(j>0)
+                    left = dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+        }
     }
     
-    return (int)res;
+    return dp[m-1][n-1];
     }
     
 }
