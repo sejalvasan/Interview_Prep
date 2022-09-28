5375eb608452fc69ad16865968b52f7593b5f612
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2022-09-28 13:35:16 +0530

    Time: 38 ms (84.02%), Space: 41.6 MB (92.51%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -1,16 +1,16 @@
 class Solution {
     public int numSquares(int n) {
-     int[]dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
         
-        for(int i =2;i<=n;i++){
+        int[]dp = new int[n+1];
+        dp[1]=1;
+        
+        for(int i = 2;i<=n;i++){
             int min = Integer.MAX_VALUE;
             
-            for(int j =1;j*j<=i;j++){
-                int remaining = i-j*j;
-                if(dp[remaining]<min)
-                    min = dp[remaining];
+            for(int j = 1;j*j<=i;j++){
+                int rem = i-j*j;
+                if(dp[rem]<min)
+                    min = dp[rem];
             }
             
             dp[i]=min+1;
