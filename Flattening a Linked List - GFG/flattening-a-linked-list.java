b626860c3f45e36ce4fde10bfa658fb6c98c693f
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2023-07-25 20:05:10 +0530

    Added solution - LeetHub

--- Flattening a Linked List - GFG/flattening-a-linked-list.java
+++ Flattening a Linked List - GFG/flattening-a-linked-list.java
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 import java.util.Scanner;
 import java.util.*;
 import java.io.*;
@@ -96,7 +96,8 @@ class Flatttening_A_LinkedList
 		t--;
 		}
 	}	
-}// } Driver Code Ends
+}
+// } Driver Code Ends
 
 
 /*Node class  used in the program
@@ -118,39 +119,39 @@ class Node
     the flattened linked list. */
 class GfG
 {
-    
-    Node merge(Node l1, Node l2){
-        Node res = new Node(-1);
-        Node temp = res;
-        
-        while(l1!=null && l2!=null){
-            if(l1.data<l2.data){
-                temp.bottom = l1;
-                temp = temp.bottom;
-                l1=l1.bottom;
-            }else{
-                 temp.bottom = l2;
-                temp = temp.bottom;
-                l2=l2.bottom;
-            }
-        }
-        
-        if(l1!=null)temp.bottom =l1;
-        if(l2!=null)temp.bottom =l2;
-        
-        return res.bottom;
-
-    }
     Node flatten(Node root)
     {
 	// Your code here
-	if(root == null || root.next == null)
+	if(root==null || root.next ==null)
 	return root;
 	
 	root.next = flatten(root.next);
 	
-	root = merge(root, root.next);
+	root = mergeTwo(root, root.next);
 	
 	return root;
+    }
+    
+    Node mergeTwo(Node one, Node two){
+        
+        Node temp = new Node(-1);
+        Node res = temp;
+        
+        while(one!=null && two!=null){
+            if(one.data<two.data){
+                temp.bottom = one;
+                temp = temp.bottom;
+                one = one.bottom;
+            }else{
+                temp.bottom = two;
+                temp = temp.bottom;
+                two = two.bottom;
+            }
+        }
+        
+        if(one!=null) temp.bottom = one;
+        else temp.bottom = two;
+        
+        return res.bottom;
     }
 }
