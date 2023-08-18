c881d5ff07349f4e1ebcec8d2a7c519d75589127
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2023-08-18 15:58:38 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 //Initial Template for Java
 
 import java.io.*;
@@ -26,26 +26,28 @@ class GFG{
             System.out.println(ob.knapSack(N, W, val, wt));
         }
     }
-}// } Driver Code Ends
+}
+// } Driver Code Ends
 
 
 //User function Template for Java
 
 class Solution{
-    static int knapSack(int n, int W, int val[], int wt[])
+    static int knapSack(int N, int W, int val[], int wt[])
     {
         // code here
-    int[][]dp = new int[n+1][W+1];
-         
-         for(int i =1;i<=n;i++){
-             for(int j = 1;j<=W;j++){
-                 if(wt[i-1]<=j){
-                dp[i][j]=Math.max(val[i-1]+dp[i][j-wt[i-1]], dp[i-1][j]);
-                 }else{
-                     dp[i][j]=dp[i-1][j];
-                 }
-             }
-         }
-         return dp[n][W];
-    } 
+        int[][]dp = new int[N+1][W+1];
+        
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=W;j++){
+                
+                if(wt[i-1]<=j){
+                    dp[i][j]=Math.max(val[i-1]+dp[i][j-wt[i-1]], dp[i-1][j]);
+                }else
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+        
+        return dp[N][W];
+    }
 }
