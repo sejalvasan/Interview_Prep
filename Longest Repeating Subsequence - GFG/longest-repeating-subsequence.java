2f0d0f66cf47ec079b0a2e4f9802399e6a86c0d3
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2023-08-18 19:18:06 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.java
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.java
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 //Initial Template for Java
 
 import java.util.*;
@@ -19,6 +19,7 @@ class GFG
         }
     }
 }
+
 // } Driver Code Ends
 
 
@@ -26,25 +27,29 @@ class GFG
 
 class Solution
 {
-    public int LongestRepeatingSubsequence(String str)
+    public int LongestRepeatingSubsequence(String S)
     {
-    char[] X=str.toCharArray();
-    char[] Y=str.toCharArray();
-    int m = str.length();
-      int L[][] = new int[m+1][m+1];
+
+        int m,n;
+        m = n = S.length();
+        String T = S;
+        
+        int L[][] = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0)
+                    L[i][j] = 0;
  
-    /* Following steps build L[m+1][n+1] in bottom up fashion. Note
-        that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
-    for (int i=1; i<=m; i++)
-    {
-    for (int j=1; j<=m; j++)
-    {
-          if (X[i-1] == Y[j-1] && i!=j)
-            L[i][j] = L[i-1][j-1] + 1;
-        else
-            L[i][j] = Math.max(L[i-1][j], L[i][j-1]);
+                else if (S.charAt(i - 1)
+                         == T.charAt(j - 1) && (i!=j))
+                    L[i][j] = L[i - 1][j - 1] + 1;
+ 
+                else
+                    L[i][j] = Math.max(L[i - 1][j],
+                                       L[i][j - 1]);
+            }
+        }
+          return L[m][n];
     }
-    }
-   return L[m][m];
- }
 }
