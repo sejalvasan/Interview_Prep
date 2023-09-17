318418bdc4228d87525d44cc946edff6d3c9d6b5
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2023-09-17 13:13:19 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.java
+++ Topological sort - GFG/topological-sort.java
@@ -64,28 +64,29 @@ class Solution
     static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
     {
         // add your code here
-        int[]indegree = new int[V];
         int[]topo = new int[V];
-        Queue<Integer> q = new LinkedList<>();
+        
+        int[]indegree = new int[V];
         
         for(int i=0;i<V;i++){
-            for(Integer it:adj.get(i))
-            indegree[it]++;
+            for(Integer it:adj.get(i)){
+                indegree[it]++;
+            }
         }
+        
+        Queue<Integer> q = new LinkedList<>();
         
         for(int i=0;i<V;i++){
             if(indegree[i]==0)
             q.add(i);
         }
-        
         int idx = 0;
-        
         while(!q.isEmpty()){
-            int rem = q.remove();
-            topo[idx] = rem;
+            int node = q.remove();
+            topo[idx] = node;
             idx++;
             
-            for(int it:adj.get(rem)){
+            for(int it:adj.get(node)){
                 indegree[it]--;
                 if(indegree[it]==0)
                 q.add(it);
