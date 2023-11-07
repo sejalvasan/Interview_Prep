69ff6d4ea864b65e18aa45888fc7af775c444307
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2023-11-07 12:33:26 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.java
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.java
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 import java.io.*;
 import java.util.*; 
 
@@ -31,7 +31,8 @@ class GFG{
             t--;
         }
     } 
-} // } Driver Code Ends
+} 
+// } Driver Code Ends
 
 
 
@@ -39,71 +40,56 @@ class GFG{
 class Solution 
 {
     //Function to find length of longest increasing subsequence.
-//     static int longestSubsequence(int size, int a[])
-//     {
-//   int[]lis = new int[size];
-        
-//         Arrays.fill(lis,1);
-//         int max=0;
-//         for(int i =1;i<size;i++){
-//             for(int j =0;j<i;j++){
-//                 if(a[i]>a[j] && lis[i]<lis[j]+1)
-//                 lis[i]=lis[j]+1;
-                
-//                 if(lis[i]>max)
-//                 max=lis[i];
-//             }
-//         }
-     
-//         return max;
-//     }
-// } 
-
- static int binarySearch(int arr[], int l, int h, int value)
+    static int longestSubsequence(int size, int a[])
     {
-        //if new value is greater than all array values,
-        //then it is placed at the end.
-        if(value>arr[h]) 
-            return h+1;
+        // code here
+        // int[]dp = new int[size];
+        // int omax = 0;
         
-        //binary search algorithm.    
-        while(h>l)
-        {
-            int middle = (h+l)/2;
-            if(arr[middle] == value) 
-                return middle;
+        // for(int i=0;i<size;i++){
+        //     int max = 0;
             
-            if(arr[middle] > value) 
-                h = middle;
+        //     for(int j=0;j<i;j++){
+        //         if(a[i]>a[j] && dp[j]>max)
+        //         max = dp[j];
+        //     }
             
-            else 
-                l = middle + 1;
+        //     dp[i]=max+1;
+        //     omax = Math.max(omax, dp[i]);
+        // }
+        
+        // return omax;
+        
+        int lastIndex = 0;
+        int tail[] = new int[size];
+        tail[0] = a[0];
+        
+        for(int i=1;i<size;i++){
+            int index = bs(tail, 0, lastIndex, a[i]);
+            tail[index] = a[i];
+            lastIndex = Math.max(lastIndex, index);
+        }
+        
+        return lastIndex+1;
+    }
+    
+    public static int bs(int[]tail, int l, int h, int val){
+        if(val>tail[h])
+        return h+1;
+        
+        while(l<h){
+            int mid = (h+l)/2;
+            
+            if(tail[mid]==val)
+            return mid;
+            
+            else if(tail[mid]>val)
+            h = mid;
+            
+            else
+            l = mid+1;
         }
         
         return h;
     }
-
-    //Function to find length of longest increasing subsequence.
-    static int longestSubsequence(int size, int a[])
-    {
-        //tail[i] holds the last value in a subsequence of length i+1.
-        int[] tail = new int[size];
-        tail[0] = a[0];
-        
-        //the position of last filled index in tail[].
-        int lastIndex = 0; 
-        
-        for(int i=1; i<size; i++)
-        {
-            //getting the furthest possible index for a[i].
-            int index = binarySearch( tail, 0, lastIndex, a[i] );
-            
-            tail[index] = a[i];
-            //updating lastIndex.
-            lastIndex = Math.max( lastIndex, index );
-        }
-        //returning the result.
-        return lastIndex + 1;
-    }
-
 } 
