425cad8525ae8548b9345f6127d7b477b4dec9b0
Sejal Vasan <69964629+sejalvasan@users.noreply.github.com>
2023-11-23 09:00:46 +0530

    Added solution - LeetHub

--- Maximum sum leaf to root path - GFG/maximum-sum-leaf-to-root-path.java
+++ Maximum sum leaf to root path - GFG/maximum-sum-leaf-to-root-path.java
@@ -116,19 +116,18 @@ class Solution
     public static int maxPathSum(Node root)
     {
         //code here
-      if(root.left!=null && root.right!=null){
-          int left = maxPathSum(root.left);
-          int right = maxPathSum(root.right);
-          return Math.max(left, right) + root.data;
-      }else if(root.left!=null){
-          int left = maxPathSum(root.left);
-          return left + root.data;
-      }else if(root.right!=null){
-          int right = maxPathSum(root.right);
-          return right + root.data;
-      }else{
-          return root.data;
-      }
+        if(root.left!= null && root.right!=null){
+            int left = maxPathSum(root.left);
+            int right = maxPathSum(root.right);
+            
+            return Math.max(left, right) + root.data;
+        }else if(root.left!=null){
+            return maxPathSum(root.left) + root.data;
+        }else if(root.right!=null){
+            return maxPathSum(root.right) + root.data;
+        }else{
+            return root.data;
+        }
         
     }
 }
